69d1210aad915e0aeffaa3e764189f185416b071
djibril sadoun <cyberpsycho012@gmail.com>
2026-06-03 15:18:47 -0400

    avatar2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %I[avatar])
   end
 end
