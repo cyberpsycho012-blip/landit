965a9f55694655c8d61cb4d91f1f7b05e343bdec
Beata Wierzbicka <b.wierzbicka90@gmail.com>
2026-06-03 20:07:22 -0400

    removed console from dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
   def after_sign_in_path_for(resource)
     dashboard_path
   end
